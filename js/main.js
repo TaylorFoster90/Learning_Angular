d0a14e109a24c1e24e75591de8eb390b2864580c
Taylor Foster <tagfoster90@gmail.com>
2015-02-26 15:08:27 -0500

    got live data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-(function (){
-	
+(function(){
+    angular.module('tiy-catalog', [ ])
+    .controller('MainController',[ '$http', function($http){
+        var self = this;
+        $http.jsonp('https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_CALLBACK&api_key=q4ubii6kukovuc0hl2e8myxx')
+        .success(function(data){
+            self.results = data.results;
+        })
+    } ]) 
+; 
 })();
