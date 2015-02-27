3b0b91e481b1a10279fbd1aab9933e6dde6965ae
Taylor Foster <tagfoster90@gmail.com>
2015-02-26 21:50:11 -0500

    populating data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 (function(){
     angular.module('tiy-catalog', [ ])
     .controller('MainController',[ '$http', function($http){
-        var self = this;
+        var dad = this
         $http.jsonp('https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_CALLBACK&api_key=q4ubii6kukovuc0hl2e8myxx')
         .success(function(data){
-            self.results = data.results;
+            dad.results = data.results;
+            dad.listing_id = _.map(data.results, "listing_id")
+            dad.description = _.map(data.results, "description")
         })
     } ]) 
 ; 
