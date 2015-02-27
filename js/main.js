0db8e9e179c1338405485a78f01a00e64f50a3e5
Taylor Foster <tagfoster90@gmail.com>
2015-02-26 22:30:56 -0500

    templat

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-(function(){
-    angular.module('tiy-catalog', [ ])
-    .controller('MainController',[ '$http', function($http){
-        var dad = this
-        $http.jsonp('https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_CALLBACK&api_key=q4ubii6kukovuc0hl2e8myxx')
-        .success(function(data){
-            dad.results = data.results;
-            dad.listing_id = _.map(data.results, "listing_id")
-            dad.description = _.map(data.results, "description")
-        })
-    } ]) 
-; 
+(function() {
+    angular.module('tiy-catalog', [])
+        .controller('MainController', ['$http',
+            function($http) {
+            	/* API call for etsy trending 25 items */
+                var API = {
+                    'url': 'https://openapi.etsy.com/v2/listings/',
+                    'category': 'trending',
+                    'jsonp': '.js?callback=JSON_CALLBACK',
+                    'apiKey': 'q4ubii6kukovuc0hl2e8myxx',
+                    'fields': 'title,description,price,currency_code',
+                    'mainImage': '&includes=MainImage'
+                };
+                var apiTemplate = _.template('${url}${category}${jsonp}&api_key=${apiKey}&fields=${fields}${mainImage}');
+                var self = this
+                $http.jsonp(apiTemplate(API))
+                    .success(function(data) {
+                        self.results = data.results;
+                        self.listing_id = _.map(data.results, "listing_id")
+                        self.title = _.map(data.results, "title")
+                    })
+            }
+        ]);
 })();
