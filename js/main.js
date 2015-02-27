ca79d99ccbd2da0b89be42e356450a5fcc313880
Taylor Foster <tagfoster90@gmail.com>
2015-02-27 13:38:41 -0500

    working to get images on the page

--- js/main.js
+++ js/main.js
@@ -2,23 +2,42 @@
     angular.module('tiy-catalog', [])
         .controller('MainController', ['$http',
             function($http) {
-            	/* API call for etsy trending 25 items */
+                /* API call for etsy trending 25 items */
+                function mapData(data) {
+                    return _.map(data, function(object) {
+                        return {
+                            title: object.title,
+                            description: object.description,
+                            price: object.price,
+                            images: {
+                                fullImage: object.MainImage.url_fullxfull,
+                                smallImage: object.MainImage.url_170x135
+                            }
+                        }
+                    })
+                }
                 var API = {
                     'url': 'https://openapi.etsy.com/v2/listings/',
                     'category': 'trending',
                     'jsonp': '.js?callback=JSON_CALLBACK',
+                    'pagination': 'limit=100&offset=0',
                     'apiKey': 'q4ubii6kukovuc0hl2e8myxx',
-                    'fields': 'title,description,price,currency_code',
+                    'fields': 'title,description,price',
                     'mainImage': '&includes=MainImage'
                 };
-                var apiTemplate = _.template('${url}${category}${jsonp}&api_key=${apiKey}&fields=${fields}${mainImage}');
+                var apiTemplate = _.template('${url}${category}${jsonp}&${pagination}&api_key=${apiKey}&fields=${fields}${mainImage}');
+                console.log(apiTemplate(API))
+
+
                 var self = this
                 $http.jsonp(apiTemplate(API))
                     .success(function(data) {
-                        self.results = data.results;
-                        self.listing_id = _.map(data.results, "listing_id")
-                        self.title = _.map(data.results, "title")
-                    })
+                            self.results = data.results;
+                            self.populate = mapData(self.results)
+                            
+                        }
+
+                )
             }
         ]);
 })();
