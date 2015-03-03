bc64a4ee1f7e1b9290a42b0dec172e27b312d9cb
Taylor Foster <tagfoster90@gmail.com>
2015-03-03 14:28:20 -0500

    yay, was able to do something with angular and todoMVC lol

---
+++ todoMvc/js/app.js
@@ -0,0 +1,25 @@
+(function (window) {
+	'use strict';
+	var todos = [
+		{
+			task: 'Taste Javascript',
+			complete: true
+		},
+		{
+			task: 'Buy a unicorn',
+			complete: false
+		}
+	]
+
+	// Your starting point. Enjoy the ride!
+	var app = angular.module('todoMvc', ['firebase']);
+
+	app.controller('MainController', [ '$firebase', function($firebase){
+		var ref = new Firebase('https://tiy-catlog.firebaseio.com/');
+		var sync = $firebase(ref);
+		this.data = sync.$asObject();
+		this.tasks = todos
+		console.log(this.data)
+	}]);
+
+})(window);
