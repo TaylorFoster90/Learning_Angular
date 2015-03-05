41e5c7cbe48914b69c4c2695af5cd558fe7eb7b4
Taylor Foster <tagfoster90@gmail.com>
2015-03-04 22:01:33 -0500

    more work with fireangular

--- todoMvc/js/app.js
+++ todoMvc/js/app.js
@@ -1,25 +1,26 @@
 (function (window) {
 	'use strict';
-	var todos = [
+var todos = [
 		{
 			task: 'Taste Javascript',
 			complete: true
 		},
 		{
-			task: 'Buy a unicorn',
-			complete: false
-		}
-	]
+		task: 'Buy a unicorn',
+		complete: false
+	}
+]
 
 	// Your starting point. Enjoy the ride!
-	var app = angular.module('todoMvc', ['firebase']);
+var app = angular.module('todoMvc', ['firebase']);
 
-	app.controller('MainController', [ '$firebase', function($firebase){
+app.controller('MainController', [ '$firebase', function($firebase){
 		var ref = new Firebase('https://tiy-catlog.firebaseio.com/');
 		var sync = $firebase(ref);
 		this.data = sync.$asObject();
-		this.tasks = todos
-		console.log(this.data)
+		this.tasks = todos;
+		this.remove = function(item){
+			_.pull(this.tasks, item)
+		};
 	}]);
-
 })(window);
