3bc451c7eb993ad499dad8bf9d61673523adc093
Taylor Foster <tagfoster90@gmail.com>
2015-03-06 23:09:02 -0500

    sooo many bugs but its getting there

--- todoMvc/js/app.js
+++ todoMvc/js/app.js
@@ -1,34 +1,29 @@
 (function (window) {
 	'use strict';
-var todos = [
-		{
-			task: 'Taste Javascript',
-			complete: true,
-		},
-		{
-		task: 'Buy a unicorn',
-		complete: false
-	}
-]
+
 
 	// Your starting point. Enjoy the ride!
 var app = angular.module('todoMvc', ['firebase']);
 
 app.controller('MainController', [ '$firebase', function($firebase){
-		var ref = new Firebase('https://tiy-catlog.firebaseio.com/');
+		var ref = new Firebase('https://tiy-catlog.firebaseio.com/todoMvc');
 		var sync = $firebase(ref);
-		this.data = sync.$asObject();
-		this.tasks = todos;
+		this.data = sync.$asArray();
+		this.tasks = this.data;
 		this.remove = function(item){
-			_.pull(this.tasks, item)
+			this.tasks.$remove(item).then(console.log('removed '+ item.task + ' from the list'));
+
 		};
-		this.markAll = function(){
-			_.forEach(this.tasks, function(object){
-					return object.complete =! object.complete
-			})
-		}
+		this.markAll = function(complete){
+			_.forEach(this.data, function(object){
+					object.complete = complete
+		})
+	}
 		this.add = function(todo){
-			this.tasks.push({task: todo, complete: false})
+			this.data.$add({task:todo, complete: false}).then(console.log('added '+todo+' to the list'))
+		};
+		this.editing= function(todo){
+			todo.edit = true;
 		}
 	}]);
 })(window);
